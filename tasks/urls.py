a9a0c15f61d4d0152a0003123ff01b793df02127
ronny karani <karanironny25@gmail.com>
2021-03-31 11:42:24 +0300

    login functionality added

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 # from . import views
-from .views import TaskDetail, TaskList, TaskCreate, TaskUpdate, TaskDelete
+from .views import CustomLoginView, TaskDetail, TaskList, TaskCreate, TaskUpdate, TaskDelete
 
 urlpatterns = [
     # path('', views.index, name='list'),
     # path('update_task/<str:pk>/', views.updateTask, name='update_task'),
     # path('delete/<str:pk>/', views.deleteTask, name='delete'),
+    path('login/', CustomLoginView.as_view(), name='login'),
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>', TaskDetail.as_view(), name='task'),
     path('task-create/', TaskCreate.as_view(), name='task-create'),

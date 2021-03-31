c4da0f0c7c32c758393b77e30ed685de9771c923
ronny karani <karanironny25@gmail.com>
2021-03-31 11:16:39 +0300

    finished CRUD functionality

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 # from . import views
-from .views import TaskDetail, TaskList, TaskCreate
+from .views import TaskDetail, TaskList, TaskCreate, TaskUpdate, TaskDelete
 
 urlpatterns = [
     # path('', views.index, name='list'),
@@ -9,5 +9,6 @@ urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>', TaskDetail.as_view(), name='task'),
     path('task-create/', TaskCreate.as_view(), name='task-create'),
-
+    path('task-update/<int:pk>', TaskUpdate.as_view(), name='task-update'),
+    path('task-delete/<int:pk>', TaskDelete.as_view(), name='task-delete'),
 ]  

e16c675700664faeb3d8218b299bbdf306a7ea35
ronny karani <karanironny25@gmail.com>
2021-03-31 11:54:27 +0300

    restricting users

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 # from . import views
 from .views import CustomLoginView, TaskDetail, TaskList, TaskCreate, TaskUpdate, TaskDelete
 
+
 urlpatterns = [
     # path('', views.index, name='list'),
     # path('update_task/<str:pk>/', views.updateTask, name='update_task'),
--- tasks/views.py
+++ tasks/views.py
@@ -3,34 +3,46 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
-from .models import *
-from .forms import *
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Task
+'''from .models import *
+from .forms import *'''
 
 # Create your views here.
 # class based views
-class TaskList(ListView):
+class CustomLoginView(LoginView):
+    template_name = 'tasks/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('tasks')
+
+class TaskList(LoginRequiredMixin, ListView):
     model = Task 
     context_object_name = 'tasks' # rename object_list
 
-class TaskDetail(DetailView):
+class TaskDetail(LoginRequiredMixin, DetailView):
     model = Task
     context_object_name = 'task'
     template_name = 'tasks/task.html'# rename the original django template name
 
-class TaskCreate(CreateView):
+class TaskCreate(LoginRequiredMixin, CreateView):
     model = Task
     fields = '__all__'
     success_url = reverse_lazy('tasks')
 
-class TaskUpdate(UpdateView):
+class TaskUpdate(LoginRequiredMixin, UpdateView):
     model = Task
     fields = '__all__'
     success_url = reverse_lazy('tasks')
 
-class TaskDelete(DeleteView):
+class TaskDelete(LoginRequiredMixin, DeleteView):
     model = Task
     context_object_name = 'task'
     success_url = reverse_lazy('tasks')
+
 
 # function based views
 '''
